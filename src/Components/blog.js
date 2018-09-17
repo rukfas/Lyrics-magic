f7bdef3334921234d942a9d40157f48496c89007
rukfas94 <farukbruncevic@rocketmail.com>
2018-09-17 03:20:13 +0200

    Added input file with onChange function

--- src/Components/blog.js
+++ src/Components/blog.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Blog extends Component {
+
     state = {
         podatci: [],
-        kralj: {}
-
+        kralj: {},
+        SongText: '',
+        ucitaniPodaci: false
     }
 
 
     componentDidMount() {
 
-        //axios.defaults.headers.common['apikey'] = 'e4de8d76db968f3f531a30e734222090';
-        axios.get("https://api.musixmatch.com/ws/1.1/track.search?q_lyrics= licna karta&page_size=3&page=1&s_track_rating=desc", {
+        this.proba();
+    }
+
+    proba() {
+        axios.get(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${this.state.SongText}&page_size=3&page=1&s_track_rating=desc`, {
             params: {
                 apikey: 'e4de8d76db968f3f531a30e734222090',
                 //q_artist: "Ceca",
@@ -20,28 +25,35 @@ class Blog extends Component {
             }
         }).then(response => {
             this.setState({
-                podatci: response.data.message.body.track_list
+                ...this.state,
+                podatci: response.data.message.body.track_list,
+                ucitaniPodaci: true
             }, () => {
-                this.nesto();
             });
         })
-
     }
 
-    nesto() {
-        console.log(this.state.podatci);
+    onChangeHandler(e) {
+        this.setState({
+            ...this.state,
+            SongText: e.target.value
+        }, () => {
+
+            this.proba();
+        })
     }
     render() {
-        console.log(this.props);
+        console.log(this.state);
         var nesto = this.state.podatci.map(a => {
             return (<p>{a.track.artist_name}</p>)
         })
-        if (this.state.podatci.length < 1) {
+        if (this.state.ucitaniPodaci === false) {
             return <p>Loading...</p>
         } else {
             return (
 
                 <div>
+                    <input type="text" value={this.state.SongText} onChange={this.onChangeHandler.bind(this)}></input>
                     {nesto}
                 </div >
             );
